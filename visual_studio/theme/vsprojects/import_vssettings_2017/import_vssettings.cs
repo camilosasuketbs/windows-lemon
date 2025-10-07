3301347fe7d23ec962f13a4d49c4b7a3f3b27b8e
Camilo Sasuke Thomas Borregaard Sørensen <camilosasuketbs@gmail.com>
2025-10-07 14:43:01 -0300

    🌌🌍 <3ThomasBSILoveYou!! 🌌🌍 [ Windows ] [ day 305 ] [ C++ ] resident Multimedia Bot <3ThomasBSJegElskerDig!!
    <3tbs, Mummi and bilbo!!
    
    Thomas Borregaard Sørensen \infinity,-0.16091989,\infinity ONE-MAN ABSOLUTE <3!! I love you, by ???-0.02041977-???write my history please make me please create me for you for me for you for me Camilo Sasuke Thomas Borregaard Sørensen!!
    
    Thomas 3 private commits on mid Dec2020!!
    
    Thomas Online YouTube VODs contribution!!
    
    Mummi 2021-11-19 01:01 (AM) Second Super Tuna Sandwich in Baguete with Lots Tomato, second Coke and cake no gluten with tea....
    
    Mummi orange-rice-flour cake on 20-Dec!!
    
    Mummi (tinytaura) watching and chatting contribution!!
    
    bilbo sleeping and needing/requesting/crying for help care (for the right person (me), the cats wanna fight with him) contribution!!
    
    camandcode (Cameron White) streaming & friendship contribution!!
    
    boflux (Spoofh, Benjamin Kuhl, from Germany) chatting contribution!!
    
    lowluds (from Canada) chatting and general support contribution!!
    
    smokesess (from Canada) chatting and general support contribution!!
    
    shegotmad (at London) chatting, learning C++ and talking about ca2 contribution!!
    
    jusg_fpga (fpga_guru, vue_equalizer, just_fpga, Oliver Pohl) chatting contribution!!
    
    tomrandall streaming Ludum contribution!!
    
    alfredotigolo and Okbatgames chatting contribution!!
    
    xmetrix streaming contribution!!
    
    JessicaMak and marcobrunodev streaming contribution!!
    
    whyyyyyyysoserious streaming chess contribution!!
    
    fpga_guru (vue_equalizer, Oliver from Deutsch) C++/ca2 interest contribution!!
    
    Myrkee (Valheim) streaming contribution!!
    
    EinfachUwe42 streaming contribution!!
    
    JPCdk streaming contribution!!
    
    sodapoppin and friends contribution!!
    
    cmgriffing streaming contribution!!
    
    TimBeaudet (Friends: FletcherLabs, tsjost and Jabokoe) streaming contribution!!
    
    sodapoppin streaming contribution!!
    
    Roxkstar74 sleeping streaming contribution!!
    
    kissloryshy chatting contribution!!
    
    jmcmorris (Jason Morris, SiegeGames) streaming contribution!!
    
    Naysayer88 (j_blow) and friends contribution!!
    
    PocketWatch (Joseph Riedel 'nd Andy Schatz) streaming C++ contribution!!
    
    strager streaming C++ contribution!!
    
    JessicaMak streaming contribution!!
    
    WTSRetro/SpiffyDane and Myrkee streaming contribution!!
    
    lastmiles streaming farwest contribution!!
    
    zeeth (and Karlos and friends from Danmark) streaming contribution!!
    
    deniz_1418 (and friends fra Danmark) streaming contribution!!
    
    rwxrob streaming contribution!!
    
    rw_grim streaming contribution!!
    
    phanxgames (Henri Price) streaming contribution!!
    
    PandemicMoon (Dunno, P4ndaExpress, Andreas Pedersen from Denmark) chatting and streaming contribution!!
    
    Zorchenhimer streaming contribution!!
    
    diegobrando_linux (Bl4ck_gookoo) chatting contribution!!
    
    jhovgaard streaming contribution!!
    
    Klay4_ chatting contribution!!
    
    HonestDanGames streaming contribution!!
    
    NorthSeaHero streaming contribution!!
    
    GexYT streaming contribution!!
    
    PirateSoftware streaming contribution!!
    
    EvanMMO streaming contribution!!
    
    OfficialAndyPyro streaming contribution!!
    
    Primeagen streaming contribution!!
    
    Jaxstyle and friends streaming contribution!!
    
    Melchizedek6809 and btcfly streaming contribution!!
    
    hvalen_hvalborg112 streaming contribution!!
    
    SomewhatAccurate C++ streaming contribution!!
    
    VerFX streaming contribution!!
    
    Listening to Bryan Adams, Westlife, Shayne Ward, MLTR, Backstreet Boys, Boyzone - Best Love Songs Ever by Relax Song at YouTube!!
    
    -- hi5 contribution...!!
    
    bilbo machine running Windows 10 Pro!!
    
    Signed-off-by: Camilo Sasuke Thomas Borregaard Sørensen <camilosasuketbs@gmail.com>

---
+++ visual_studio/theme/vsprojects/import_vssettings_2017/import_vssettings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace import_vssettings
+{
+
+    class Program
+    {
+
+        static int Main(string[] args)
+        {
+
+            try
+            {
+
+                import_vssettings(args[0]);
+
+            }
+            catch (Exception)
+            {
+
+                return 666;
+
+            }
+
+            return 0;
+
+        }
+
+        static void import_vssettings(string strFileName)
+        {
+
+            var dte = (EnvDTE.DTE)System.Runtime.InteropServices.Marshal.GetActiveObject("VisualStudio.DTE"); // version neutral
+
+            dte.ExecuteCommand("Tools.ImportandExportSettings", "/import:" + strFileName);
+
+        }
+
+    }
+
+}
---
+++ visual_studio/theme/vsprojects/visual_studio_automation_2017/visual_studio_automation_2017.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace import_vssettings
+{
+
+    class Program
+    {
+
+        static int Main(string[] args)
+        {
+
+            try
+            {
+
+                if (args.Count() >= 4)
+                {
+
+                    //automate_visual_studio4(args[0], args[1], args[2], args[3]);
+
+                }
+                else
+                {
+
+                    automate_visual_studio(args[0]);
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                //MessageBox.Show(e.ToString(), "Visual Studio Automation 2017");
+                return 666;
+
+            }
+
+            return 0;
+
+        }
+
+        static void automate_visual_studio(string strFileName)
+        {
+
+            string strImportVsSettings = "importvssettings://";
+
+            if (strFileName.StartsWith(strImportVsSettings))
+            {
+
+                strFileName = strFileName.Substring(strImportVsSettings.Length);
+
+                import_vs_settings(strFileName);
+
+                return;
+
+            }
+
+            string strOpenVsProject = "openvsproject://";
+
+            if (strFileName.StartsWith(strOpenVsProject))
+            {
+
+                strFileName = strFileName.Substring(strOpenVsProject.Length);
+
+                open_vs_project(strFileName);
+
+                return;
+
+            }
+
+        }
+
+        static void import_vs_settings(string strFileName)
+        {
+
+            var dte = (EnvDTE.DTE)System.Runtime.InteropServices.Marshal.GetActiveObject("VisualStudio.DTE"); // version neutral
+
+            dte.ExecuteCommand("Tools.ImportandExportSettings", "/import:" + strFileName);
+
+        }
+
+        static bool IsSameFile(string file1, string file2)
+        {
+            return Path.GetFullPath(file1).Equals(Path.GetFullPath(file2), StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        static void open_vs_project(string strFileName)
+        {
+
+            var dte = (EnvDTE.DTE)System.Runtime.InteropServices.Marshal.GetActiveObject("VisualStudio.DTE"); // version neutral
+
+
+            dte.Solution.Properties.Item("StartupProject").Value = strFileName;
+            //dte.ExecuteCommand("Project.SetasStartUpProject", "/import:" + strFileName);
+            ///foreach (EnvDTE.Project proj in (Array)dte.Solution.Projects)
+            {
+
+                // if (IsSameFile(proj.FileName, strFileName))
+//                {
+//                    MessageBox.Show(dte.Solution.FullName, "Solution Name", MessageBoxButtons.OK,
+//           MessageBoxIcon.Asterisk);
+
+//                    string s = Path.GetFileNameWithoutExtension(strFileName);
+
+//                    string[] sa = new string[] { s };
+
+//                    //dte.Solution.SolutionBuild.StartupProjects.Clear;
+
+//                    //dte.Solution.SolutionBuild.StartupProjects.add(proj);
+
+//                    dte.Solution.SolutionBuild.StartupProjects = sa;
+
+//                    MessageBox.Show(sa[0], "Startup Projects", MessageBoxButtons.OK,
+//MessageBoxIcon.Asterisk);
+
+//                    // break;
+
+//                }
+
+            }
+
+        }
+
+        //static void automate_visual_studio4(string str1, string str2, string str3, string str4)
+        //{
+
+        //    if (str1 == "ca2project")
+        //    {
+
+        //        if (str4 == "action=open")
+        //        {
+
+        //            char[] charSeparators = new char[] { '/' };
+
+        //            string[] result = str3.Split(charSeparators);
+
+        //            string str = "C:\\" + str2 + "\\" + result[0] + "\\appseed\\" + result[1]
+
+        //            dte.ExecuteCommand("Tools.ImportandExportSettings", "/import:" + strFileName);
+
+        //        }
+
+        //    }
+
+        //}
+
+    }
+
+}
